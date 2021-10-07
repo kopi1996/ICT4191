ab30c2c09c7e45ab4eee0711de9b793339f7df98
kopi1996 <kopipakee@gmail.com>
2021-10-08 00:45:38 +0530

    Orm Exercise 1

--- OrmExercise/src/main/java/com/example/demo/OrmExerciseApplication.java
+++ OrmExercise/src/main/java/com/example/demo/OrmExerciseApplication.java
@@ -1,13 +1,13 @@
-package com.gopinath.DoctorPatient;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoctorPatientApplication {
+public class OrmExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoctorPatientApplication.class, args);
+		SpringApplication.run(OrmExerciseApplication.class, args);
 	}
 
 }
--- OrmExercise/src/test/java/com/example/demo/OrmExerciseApplicationTests.java
+++ OrmExercise/src/test/java/com/example/demo/OrmExerciseApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sharmi.DoctorPatient;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DoctorPatientApplicationTests {
+class OrmExerciseApplicationTests {
 
 	@Test
 	void contextLoads() {
